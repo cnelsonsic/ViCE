0bb2a3cc056072b3dd05cc4cbcee741b52d0bfcb
Edwin Marshall <emarshall85@gmail.com>
2012-02-11 01:04:09 -0600

    - added new classmethod for actions, which can be used as a decorator as well

--- src/vice/plugins/__init__.py
+++ src/vice/plugins/__init__.py
@@ -27,6 +27,16 @@ class Plugin(object):
 
 class Action(Plugin):
 
+    @classmethod
+    def new(cls, function):
+        return type(function.__name__, (cls,),
+                    Dict(NAME=function.__name__lower(), __call__=function))
+
+    @classmethod
+    def plugins(cls, *args, **kwargs):
+        return Dict((plugin_name, plugin(*args, **kwargs)) for plugin_name, plugin
+                    in super(Action, cls).plugins().iteritems())
+
     def __call__(self):
         raise NotImplementedError("All actions should implement __call__!")
 
@@ -36,7 +46,8 @@ class Item(Plugin):
 
     @classmethod
     def new(cls, name, attributes):
-        return type(name, (cls,), dict(NAME = name.lower(), ATTRIBUTES=attributes))
+        return type(name, (cls,),
+                    dict(NAME = name.lower(), ATTRIBUTES=attributes))
 
     def __init__(self, *args, **kwargs):
         self.attributes = Dict()
