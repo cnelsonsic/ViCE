17b3f0126123ee828c94f608db67992f54acc7b7
Edwin O Marshall (aspidites) <emarshall85@gmail.com>
2012-02-13 18:54:23 -0600

    - added games plugin directory for use later - __init__.py describes how this directory should be used

---
+++ src/vice/plugins/games/__init__.py
@@ -0,0 +1,3 @@
+# The official WTactics plugin will be placed her upon completion.
+# this should also be the system-wide install location for all plugins.
+# User-specific installs should probably go in XDG_CONFIG_HOME/vice/plugins/games
