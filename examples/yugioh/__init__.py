63a34322b8f2aa92f28f5c39a2a0618589e3b718
Edwin O Marshall (aspidites) <emarshall85@gmail.com>
2012-04-12 22:56:54 -0700

    - deleted duplicate nested vice subdir

--- examples/yugioh/__init__.py
+++ examples/yugioh/__init__.py
@@ -1,9 +1,4 @@
 # -*- encoding: utf-8 -*-
 
-# vice plugin imports
-
-# yugioh plugin imports
-# from yugioh import items, actions
-
-name = "Yu-Gi-Oh"
-version = "0.1.0"
+name = 'Yu-Gi-Oh'
+version = '0.1.0'
