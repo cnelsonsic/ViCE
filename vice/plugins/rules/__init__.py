249901cbdbd7c479e60f674970c3a54d9ef90317
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-04-25 10:00:39 -0700

    - forgot copyright line in notices

--- vice/plugins/rules/__init__.py
+++ vice/plugins/rules/__init__.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2011-2012 Edwin Marshall <emarshall85@gmail.com>
+#
 # This file is part of ViCE.
-
+#
 # ViCE is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # ViCE is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with ViCE.  If not, see <http://www.gnu.org/licenses/>.
-
 
 # The official WTactics plugin will be placed her upon completion.
 # this should also be the system-wide install location for all plugins.
