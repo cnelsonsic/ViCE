dc7f5945a321cf1ea20d96b56d13ff65b6ca922b
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-05-03 00:42:39 -0700

    - fixed copyright info in documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ViCE'
-copyright = '2012, Edwin O Marshall'
+copyright = '2011-2012, Edwin O Marshall'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
