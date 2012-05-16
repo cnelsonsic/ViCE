d7914d096eab27165ca26b8fb76bdf06dcabedea
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-05-16 15:09:05 -0700

    -cleaned up a dir structure

---
+++ doc/source/preface.rst
@@ -0,0 +1,48 @@
+Preface
+########
+
+ViCE is an open source, modular framework for playing and creating Trading 
+Card Games (TCGs). This manual describes the various parts of the framework from
+three user perspectives, and also includes reference material for ViCE's API.
+
+.. note::
+    While there are subtle differences between the Trading Card Games and
+    Collectible Card Games, in the interest of simplicity, this manual refers
+    to them collectively (no pun intended) as TCGs. 
+
+The Roles
+=========
+ViCE is a framework suitable for use by users of varying interests and 
+technical backgrounds. In an effort to help facilitate learning, three key
+user roles have been identified, as follows:
+
+* Players: These are the individuals who aren't concerned with how ViCE's 
+  internals work, and could care less about creating new or porting existing 
+  TCGs to the framework. There sole reason for using ViCE is to play these
+  games. 
+
+* Designers: These are the individuals who are not interested in the
+  implementation details of ViCE's internals, but would like to learn
+  enough to enable them to either create new TCGs or port existing ones to
+  ViCE.
+ 
+* Developers: These are the individuals who are interested in *how* ViCE works,
+  for the sake of implementing new features not possible through the currently
+  available API or fixing bugs.
+
+While we have defined three distinct roles, these roles are not mutually
+exclusive, and can be viewed as somewhat hierarchial. That is, an individual
+who categorizes himself as a developer may also categorize himself as a
+designer and player.
+
+How This Manual is Organized
+============================
+
+This book is organized into four parts, the first three of which being guides
+modelled after the roles identified above:
+
+.. toctree::
+    Part I: player_guide/intro.rst
+    Part II: designer_guide/intro.rst
+    Part III: developer_guide/intro.rst
+    Part IV: api_reference.rst 
--- vice/database.py
+++ vice/database.py
@@ -30,10 +30,10 @@ def integer(**kwargs):
     return kwargs
 
 class Database(object):
-    def __init__(self, URI=None, verbose=False):
+    def __init__(self, URI=None, echo=False):
         self.metadata = sqlalchemy.MetaData()
         if URI:
-            self.connect(URI, verbose)
+            self.connect(URI, echo)
         else:
             self.engine = None
 
@@ -41,14 +41,14 @@ class Database(object):
     def tables(self):
         return self.metadata.tables.keys()
 
-    def connect(self, URI, verbose=False):
-        self.engine = sqlalchemy.create_engine(URI, echo=verbose)
+    def connect(self, URI, echo=False):
+        self.engine = sqlalchemy.create_engine(URI, echo=echo)
         self.metadata.reflect(bind=self.engine)
 
         for table in self.tables:
             setattr(self, table, self.metadata.tables[table])
 
-    def create_table(self, table_name, column_attrs):
+    def create_table(self, table_name, **column_attrs):
         if table_name in self.tables:
             return
 
@@ -56,17 +56,31 @@ class Database(object):
 
         for attr in column_attrs.keys():
             columns.append(sqlalchemy.Column(attr.rstrip('_'),
-                **column_attrs[attr.rstrip('_')]
+                **column_attrs[attr]
             ))
 
         setattr(self, table_name,
             sqlalchemy.Table(table_name, self.metadata, *columns
         ))
+
         self.metadata.create_all(self.engine)
 
     def insert(self, table_name, **kwargs):
         values = {key.rstrip('_'): value for key, value in kwargs.items()}
         insert = getattr(self, table_name).insert().values(**values)
         connection = self.engine.connect()
+
         return connection.execute(insert)
 
+    def select(self, tables, **kwargs):
+        kwargs = {key.rstrip('_'): value for key, value in kwargs.items()}
+
+        if hasattr(tables, '__iter__'):
+            tables = [getattr(self, table) for table in tables]
+        else:
+            tables = [getattr(self, tables)]
+
+        select = sqlalchemy.sql.select(tables, **kwargs)
+        connection = self.engine.connect()
+
+        return connection.execute(select)
