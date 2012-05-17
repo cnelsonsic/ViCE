b2ec097a6427f50336f8853a8956d8e215c39981
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-05-16 22:27:01 -0700

    - updated database doctests - wrote documnetation for database module

--- tests/test_database_module.py
+++ tests/test_database_module.py
@@ -4,11 +4,11 @@ from vice.database import integer, string, Database
 
 class TestSQLiteDatabase(unittest.TestCase):
     def setUp(self):
-        self.db = Database('sqlite:///wtactics.sqlite')
+        self.db = Database('sqlite:///:memory:')
         self.test_table_creation()
 
     def test_table_creation(self):
-        result = self.db.create_table('cards',
+        self.db.create_table('cards',
             id = integer(primary_key=True),
             name = string(),
             def_ = integer(),
@@ -20,19 +20,20 @@ class TestSQLiteDatabase(unittest.TestCase):
         assert 'cards' in self.db.tables, self.db.tables
         assert sorted(column_names) == ['atk', 'def', 'id', 'name'], sorted(column_names)
 
-    def test_table_insertion(self):
-        self.db.insert('cards',
+    def test_simple_table_selection(self):
+        self.test_record_creation()
+        result = self.db.select('cards')
+
+        assert result, result
+
+    def test_record_creation(self):
+        result = self.db.create_record('cards',
             name = 'Imp',
-            atk = 4,
-            def_ = 4
+            atk = 2,
+            def_ = 2
         )
 
-    def test_simple_table_selection(self):
-        self.test_table_insertion()
-        results = self.db.select('cards')
-
-        assert results, results
-
+        assert result, result
 
 if __name__ == '__main__':
     unittest.main()
