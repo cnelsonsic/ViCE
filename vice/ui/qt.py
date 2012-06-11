91f9406bd2a04b1f4e36ea4219034c7dcb8f54fd
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-06-11 15:48:32 -0700

    - learned about components in qml

--- vice/ui/qt.py
+++ vice/ui/qt.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 import sys
 from PySide import QtCore, QtGui, QtDeclarative
+
+qml_dir = os.path.join(os.path.dirname(__file__), 'qml')
 
 class MainWindow(QtDeclarative.QDeclarativeView):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
-
-        filename = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), 'qml', 'view.qml')
-        )
-        self.setWindowTitle("Main Window")
-        self.setSource(filename)
-        self.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
+        self.setSource(os.path.abspath(os.path.join(qml_dir, 'main.qml')))
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
