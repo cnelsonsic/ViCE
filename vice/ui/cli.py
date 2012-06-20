0cc9ea748262956307e55fd135f77965f64803b7
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-06-20 14:31:31 -0700

    - started implementing cli interface to vice

---
+++ vice/ui/cli.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser(description="ViCE's Command Line Interface")
+    subparsers = parser.add_subparsers()
+
+    create_parser = subparsers.add_parser('create',
+        help='Create various plugins and their components.')
+    #create_parser.add_argument('game',
+    #    help='create a skeleton for a game plugin')
+    create_parser.add_argument('database',
+        help='create a database')
+    create_parser.add_argument('item',
+        help='create a skeleton for an item plugin')
+    create_parser.add_argument('action',
+        help='create a skeleton for an action plugin')
+    create_parser.add_argument('container',
+        help='create a skeleton for an container plugin')
+    create_parser.add_argument('--interactive', '-i',
+        help='create component interactively')
+
+    parser.parse_args()
+
+if __name__ == '__main__':
+    main()
