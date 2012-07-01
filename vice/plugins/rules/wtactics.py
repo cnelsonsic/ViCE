f93e3f87be9f732e4786a2b9eaaf23ded7ceda87
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-07-01 11:36:38 -0700

    - fixed container definitions

--- vice/plugins/rules/wtactics.py
+++ vice/plugins/rules/wtactics.py
@@ -9,29 +9,35 @@
 from vice.plugins import Item, Container, Action
 from vice.database import Database
 
+"""
 db = Database('wtactics.db')
 
 # Items
 Item.from_table('Card', 'cards', exclude=(
     'border_color', 'footer'
 ))
+"""
 
 Item.new('Token', attributes=(
     'owner', 'type_', 'target' # target is which card it is placed on
 ))
 
 # Containers
-Zone = Container.new('Zone', lambda self, item:
-    item.NAME == 'Card'
-)
+Zone = Container.new('Zone', lambda cls, item: [
+    item.NAME == 'Card',
+])
 
 for zone in 'Questing', 'Offensive', 'Defensive':
-    Zone.new('{0}Zone'.format(zone), lambda self, item:
-        Zone.consraints + [
-            zone.lower() in item.types
+    Zone.new('{0}Zone'.format(zone), lambda cls, item:
+        Zone.constraints(cls, item) + [
+           zone.lower() in item.types
     ])
 
-HeroZone = Zone.new('HeroZone', lambda self, item:
-    Zone.constraints + [
-        len(self) == 1
+HeroZone = Zone.new('HeroZone', lambda cls, item:
+    Zone.constraints(cls, item) + [
+        len(cls) == 1
 ])
+
+actions = Action.plugins()
+containers = Container.plugins()
+items = Item.plugins()
