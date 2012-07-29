87a75895927b7f47876f8449c9bcf1c96aa504d3
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-07-29 09:21:26 -0500

    - began implementing 'doc' command for setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
 from setuptools import setup, find_packages
+from setuptools import Command
+
+class Doc(Command):
+    """Command to generate documentation"""
+    """ TODO:
+        * add --sphinxopts command to replace SHINXOPTS
+        * add --sphinxbuild command to set sphinxbuild command
+        * add --builddir command to set build directory
+    """
+
+    description = "Generate documentation"
+
+    user_options = [
+        ('formats=', None,
+         'formats for documentation (comma-separated list)')]
+
+    def initialize_options(self):
+        self.formats = None
+
+    def finalize_options(self):
+        if self.formats is None:
+            self.formats = ['html']
+        else:
+            self.formats = [
+                format.strip() for format in self.formats.split(',')]
+
+    def run(self):
+        import os
+        import sphinx
+
+        sphinxbuild = os.environ.get('SPHINXBUILD', 'sphinx-build')
+        args = '{0} -b {1} -d ./build/doctere -W ./doc/source build'
+
+        for fmt in self.formats:
+            sphinx.main(args.format(sphinxbuild, fmt).split())
+
 setup(
     name='ViCE',
     version='0.0.1',
@@ -20,5 +56,8 @@ setup(
     ],
     packages=find_packages(exclude=['*test*']),
     install_requires=['SQLAlchemy>=0.7.6'],
-    test_suite="tests"
+    test_suite='tests',
+    cmdclass={
+        'doc': Doc
+    },
 )
