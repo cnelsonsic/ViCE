9c7fcfaad5dfb77a0edc29eeee5eb78decfee505
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-08-05 12:43:53 -0500

    - finished implementing basic doc support for setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 from setuptools import Command
 from setuptools.command.test import test
@@ -15,40 +16,84 @@ class PyTest(test):
 
 class Doc(Command):
     """Command to generate documentation"""
-    """ TODO:
-        * add --sphinxopts command to replace SHINXOPTS
-        * add --sphinxbuild command to set sphinxbuild command
-        * add --builddir command to set build directory
-        * rewrite process_images.sh in python
-    """
 
     description = "Generate documentation"
 
     user_options = [
         ('formats=', None,
-         'formats for documentation (comma-separated list)')]
+         'formats for documentation (comma-separated list)'),
+        ('builddir=', None,
+         'directory in which to build documentation'),
+        ('sourcedir=', None,
+         'directory where documentation source exists'),
+        ('sphinxbuild=', None,
+         'name of sphinx-build executable'),
+        ('sphinxopts=', None,
+         'options to pass to sphinx-build')]
 
     def initialize_options(self):
         self.formats = None
+        self.builddir = None
+        self.sourcedir = None
+        self.sphinxbuild = None
+        self.sphinxopts = None
 
     def finalize_options(self):
-        if self.formats is None:
+        if not self.formats:
             self.formats = ['html']
         else:
             self.formats = [
                 format.strip() for format in self.formats.split(',')]
 
+        if not self.builddir:
+            self.builddir = 'build'
+
+        if not self.sourcedir:
+            self.sourcedir = os.path.join('doc', 'source')
+
+        if not self.sphinxbuild:
+            self.sphinxbuild = os.path.join(
+                os.environ.get('PYTHONPATH', './'), 'sphinx-build')
+
+        if not self.sphinxopts:
+            self.sphinxopts = '-W'
+
     def run(self):
-        import os
         import sphinx
+
+        sphinxbuild = os.environ.get('SPHINXBUILD', self.sphinxbuild)
+        sphinxopts = os.environ.get('SPHINXOPTS', self.sphinxopts)
+        args = '{0} -b {1} -d {2}/doctree {3} {4} {2}/{1}'
+
+        self._process_images()
+        for fmt in self.formats:
+            sphinx.main(args.format(sphinxbuild, fmt, self.builddir, sphinxopts, self.sourcedir).split())
+
+    def _process_images(self):
+        return
+        import shutil
         from subprocess import call
 
-        sphinxbuild = os.environ.get('SPHINXBUILD', 'sphinx-build')
-        args = '{0} -b {1} -d ./build/doctere -W ./doc/source build'
+        imagedir = os.path.join(self.sourcedir, '_static')
 
-        for fmt in self.formats:
-            call(['./doc/process_images.sh'])
-            sphinx.main(args.format(sphinxbuild, fmt).split())
+        # create class and pacakge diagrams
+        args = 'pyreverse -my -o svg -p ViCE vice'
+        call(args.split())
+
+        for image in os.listdir('.'):
+            if filename.endswith('.svg'):
+                try:
+                    shutil.move(filename, imagedir)
+                except IOError:
+                    pass
+
+        # convnert svgs to pngs
+        args = 'inkscape -D -e {0} {1}'
+        [call(args.format(
+            os.path.join(imagedir, filename.replace('svg', 'png')),
+            os.path.join(imagedir, filename)).split())
+            for filename in os.listdir(imagedir) if filename.endswith('.svg')]
+
 
 setup(
     name='ViCE',
@@ -67,13 +112,10 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Games/Entertainment :: Board Games',
         'Topic :: Games/Entertainment :: Turn Based Strategy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+        'Topic :: Software Development :: Libraries :: Python Modules'],
     packages=find_packages(exclude=['*test*']),
     install_requires=['SQLAlchemy>=0.7.6'],
     tests_require=['pytest'],
     cmdclass={
         'doc': Doc,
-        'test': PyTest
-    },
-)
+        'test': PyTest})
