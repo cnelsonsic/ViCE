d0638a4d88d9d4ad23df3bbc8ec1fb4c8057dda6
Edwin Marshall (aspidites) <emarshall85@gmail.com>
2012-09-25 18:55:38 -0400

    - wrote stubs for command line interface

--- vice/ui/command.py
+++ vice/ui/command.py
@@ -1,48 +1,28 @@
 from cmd import Cmd
-
-plugin_template = """\
-import os
-import sys
-from vice.databse import Database
-from vice.plugins import Action, Container, Item
-
-DB_FILE = {db_file}
-DB_DRIVER = 'sqlite'
-
-# DO NOT EDIT BELOW THIS LINE!
-db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), DB_FILE))
-
-if DB_DRIVER == 'sqlite':
-    db = Database('sqlite:////' + db_path)
-else:
-    print(DB_DRIVER + "driver not implemented, sorry!")
-    sys.exit(1)
-
-"""
-
-item_template = """\
-{item_name} = Item.new('{item_name}', attributes=(
-    {item_attrs}
-))
-"""
+try:
+    input = raw_input
+except NameError:
+    # python 3
+    pass
 
 class ViceCmd(Cmd):
     prompt = 'vice> '
 
-    def do_generate(self, line):
-        print line
-        with open('foo.py', 'w') as f:
-            plugin_template.format(
-                db_file='foo.db'
-            )
-            f.write(plugin_template)
+    def do_open(self, line):
+        pass
+
+    def do_new(self, line):
+        pass
+
+    def do_remove(self, line):
+        pass
 
     def do_quit(self, line):
         return True
 
     def do_EOF(self, line):
         print('')
-        return True
+        return do_quit(line)
 
 def main():
     import sys
